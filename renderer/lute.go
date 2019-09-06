af3efc57e6a8781c347527c769c15975c129fad7
Fan Yang <me@minidump.info>
2019-09-06 11:38:24 +0800

    (*)no need to inline css style for lute

--- renderer/lute.go
+++ renderer/lute.go
@@ -9,7 +9,7 @@ import (
 
 //export Lute
 func Lute(md string, style string, lineNumbers bool) *C.char {
-	luteEngine := lute.New(lute.CodeSyntaxHighlight(true, true, lineNumbers, style))
+	luteEngine := lute.New(lute.CodeSyntaxHighlight(true, false, lineNumbers, style))
 	html, err := luteEngine.MarkdownStr("KarenMeu", md)
 	if nil != err {
 		log.Println(err)
