f294fd7f0736a1cd0c2d9c18975d9c6a4d261bf9
Fan Yang <me@minidump.info>
2020-12-15 23:00:52 +0800

    (+)add script to deploy on macOS

---
+++ tools/macdeploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+otool -L client/KarenMeu.app/Contents/MacOS/KarenMeu
+$QTDIR/bin/macdeployqt client/KarenMeu.app
+cp -R 3rdparty/vtextedit/src/libs/syntax-highlighting/VSyntaxHighlighting.framework client/KarenMeu.app/Contents/Frameworks/
+cp -R 3rdparty/vtextedit/src/editor/VTextEdit.framework client/KarenMeu.app/Contents/Frameworks/
+install_name_tool -change VSyntaxHighlighting.framework/Versions/1/VSyntaxHighlighting @rpath/VSyntaxHighlighting.framework/Versions/1/VSyntaxHighlighting client/KarenMeu.app/Contents/MacOS/KarenMeu 
+install_name_tool -change VTextEdit.framework/Versions/1/VTextEdit @rpath/VTextEdit.framework/Versions/1/VTextEdit client/KarenMeu.app/Contents/MacOS/KarenMeu
+otool -L client/KarenMeu.app/Contents/MacOS/KarenMeu
