55e18f29183d9c5b3b25ad72243e90efbf0aa175
Fan Yang <missdeer@dfordsoft.com>
2021-01-05 22:53:04 +0800

    update i18n

--- client/translations/KarenMeu_zh_CN.ts
+++ client/translations/KarenMeu_zh_CN.ts
@@ -1068,7 +1068,7 @@
         <translation>PDF文档未导出到%1。</translation>
     </message>
     <message>
-        <location filename="../markdownview.cpp" line="731"/>
+        <location filename="../markdownview.cpp" line="730"/>
         <source>Metadata</source>
         <translation>元数据</translation>
     </message>
