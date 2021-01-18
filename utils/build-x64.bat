8973bd1367a7fe04104ae8354b8b9cca7ca97065
Fan Yang <fanyang@cadg.cn>
2021-01-18 12:03:19 +0800

    don't create sub diretory for utils binaries

--- utils/build-x64.bat
+++ utils/build-x64.bat
@@ -6,14 +6,14 @@ set CGO_ENABLED=1
 go build -buildmode=c-archive -o utils.a 
 gcc utils.def utils.a -shared -lwinmm -lWs2_32 -o utils.dll -Wl,--out-implib,utils.dll.a
 lib /def:utils.def /name:utils.dll /out:utils.lib /MACHINE:X64
-mkdir x64
-copy /y *.a x64\
-del /q *.a
-copy /y *.dll x64\
-del /q *.dll
-copy /y *.lib x64\
-del /q *.lib
-copy /y *.h x64\
-del /q *.h
-copy /y *.exp x64\
-del /q *.exp
+REM mkdir x64
+REM copy /y *.a x64\
+REM del /q *.a
+REM copy /y *.dll x64\
+REM del /q *.dll
+REM copy /y *.lib x64\
+REM del /q *.lib
+REM copy /y *.h x64\
+REM del /q *.h
+REM copy /y *.exp x64\
+REM del /q *.exp
--- utils/build-x86.bat
+++ utils/build-x86.bat
@@ -6,14 +6,14 @@ set CGO_ENABLED=1
 go build -buildmode=c-archive -o utils.a 
 gcc utils.def utils.a -shared -lwinmm -lWs2_32 -o utils.dll -Wl,--out-implib,utils.dll.a
 lib /def:utils.def /name:utils.dll /out:utils.lib /MACHINE:X86
-mkdir x86
-copy /y *.a x86\
-del /q *.a
-copy /y *.dll x86\
-del /q *.dll
-copy /y *.lib x86\
-del /q *.lib
-copy /y *.h x86\
-del /q *.h
-copy /y *.exp x86\
-del /q *.exp
+REM mkdir x86
+REM copy /y *.a x86\
+REM del /q *.a
+REM copy /y *.dll x86\
+REM del /q *.dll
+REM copy /y *.lib x86\
+REM del /q *.lib
+REM copy /y *.h x86\
+REM del /q *.h
+REM copy /y *.exp x86\
+REM del /q *.exp
