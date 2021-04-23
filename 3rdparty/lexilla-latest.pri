a47a4963149e0e6c36502f07734c6742625cf913
Fan Yang <fanyang@cadg.cn>
2021-04-23 10:04:58 +0800

    fix set lexer

---
+++ 3rdparty/lexilla-latest.pri
@@ -0,0 +1,38 @@
+INCLUDEPATH += $$PWD/lexilla/include \
+    $$PWD/lexilla/src \
+    $$PWD/lexilla/lexlib
+
+SOURCES += \
+    $$files($$PWD/lexilla/src/*.cxx, false)  \
+    $$files($$PWD/lexilla/lexlib/*.cxx, false)  \
+    $$files($$PWD/lexilla/lexers/*.cxx, false)
+
+HEADERS += \
+    $$files($$PWD/lexilla/include/*.h, false)
+
+RC_FILE += $$PWD/lexilla/src/LexillaVersion.rc
+
+DEFINES += SCINTILLA_QT=1 SCI_LEXER=1 _CRT_SECURE_NO_DEPRECATE=1
+CONFIG(release, debug|release) {
+    DEFINES += NDEBUG=1
+}
+
+macx {
+    QMAKE_LFLAGS_SONAME = -Wl,-install_name,@rpath/
+}
+
+OUT_FOLDER = $$absolute_path($$relative_path($$PWD, $$_PRO_FILE_PWD_), $$OUT_PWD)/lexilla/bin
+lib_name = ScintillaEdit
+macx {
+    lib_framework_name = $${lib_name}.framework
+
+    INCLUDEPATH += $${OUT_FOLDER}/$${lib_framework_name}/Headers
+    LIBS += -F$${OUT_FOLDER} -framework $${lib_name}
+} else {
+    INCLUDEPATH *= $$PWD/include
+    win32: {
+        LIBS += -L$$OUT_FOLDER -l$${lib_name}5
+    } else: {
+        LIBS += -L$$OUT_FOLDER -l$${lib_name}
+    }
+}
--- client/scintillaconfig.cpp
+++ client/scintillaconfig.cpp
@@ -1,13 +1,15 @@
+#include <QCoreApplication>
+#include <QDir>
 #include <QDomDocument>
 #include <QDomElement>
 #include <QFile>
 #include <QFontDatabase>
 #include <QStringBuilder>
-#include <QCoreApplication>
 #include <QtCore>
-#include <QDir>
 
 #include "scintillaconfig.h"
+#include "ILexer.h"
+#include "Lexilla.h"
 #include "ScintillaEdit.h"
 #include "settings.h"
 
@@ -215,28 +217,10 @@ void ScintillaConfig::applyLanguageStyle(const QString &configPath, const QStrin
     }
 
     auto l = lexer.toUtf8();
-    //m_sci->setLexerLanguage(l.data());
+    // m_sci->setLexerLanguage(l.data());
 
-#ifdef _WIN32
-    typedef void *(__stdcall *CreateLexerFn)(const char *name);
-#else
-    typedef void *(*CreateLexerFn)(const char *name);
-#endif
-
-#ifdef Q_OS_MAC
-    QDir dir(QCoreApplication::applicationDirPath());
-    dir.cdUp();
-    dir.cd("Libs");
-    QString path = dir.absolutePath();
-#else
-    QString path = QCoreApplication::applicationDirPath();
-#endif
-    QFunctionPointer fn = QLibrary::resolve(path+ "/lexilla", "CreateLexer");
-    if (fn)
-    {
-        void *lexerId = ((CreateLexerFn)fn)(l.data());
-        m_sci->setILexer((sptr_t)(void *)lexerId);
-    }
+    void *lexerId = CreateLexer(l.data());
+    m_sci->setILexer((sptr_t)(void *)lexerId);
 }
 
 void ScintillaConfig::applyThemeStyle(const QString &themePath, const QString &lang)
