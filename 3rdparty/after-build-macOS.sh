f49ba298975f169370b731c6dac0275523de2519
Fan Yang <me@minidump.info>
2021-07-07 06:45:20 +0800

    remove legacy frameworks before being copied

--- 3rdparty/after-build-macOS.sh
+++ 3rdparty/after-build-macOS.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 if [ ! -d client/KarenMeu.app/Contents/Frameworks ]; then mkdir client/KarenMeu.app/Contents/Frameworks; fi
+if [ -d client/KarenMeu.app/Contents/Frameworks/ScintillaEdit.framework ]; then rm -rf client/KarenMeu.app/Contents/Frameworks/ScintillaEdit.framework; fi
 cp -R 3rdparty/scintilla/bin/ScintillaEdit.framework client/KarenMeu.app/Contents/Frameworks/
+if [ -d client/KarenMeu.app/Contents/Frameworks/VTextEdit.framework ]; then rm -rf client/KarenMeu.app/Contents/Frameworks/VTextEdit.framework; fi
 cp -R 3rdparty/vtextedit/src/editor/VTextEdit.framework client/KarenMeu.app/Contents/Frameworks/
+if [ -d client/KarenMeu.app/Contents/Frameworks/VSyntaxHighlighting.framework ]; then rm -rf client/KarenMeu.app/Contents/Frameworks/VSyntaxHighlighting.framework; fi
 cp -R 3rdparty/vtextedit/src/libs/syntax-highlighting/VSyntaxHighlighting.framework client/KarenMeu.app/Contents/Frameworks/
