2d6d256953e234030ccdc2c6c8964d3071af2a02
Fan Yang <fanyang@cadg.cn>
2022-05-04 09:12:33 +0800

    fix scintilla version

--- tools/macdeploy.sh
+++ tools/macdeploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if [ ! -d client/KarenMeu.app ]; then
+    echo "Bundle client/KarenMeu.app doesn't exist"
+    exit 1
+fi
 otool -L client/KarenMeu.app/Contents/MacOS/KarenMeu
+echo "QTDIR=$QTDIR"
 $QTDIR/bin/macdeployqt client/KarenMeu.app
 if [ ! -d client/KarenMeu.app/Contents/Frameworks ]; then
 mkdir -p client/KarenMeu.app/Contents/Frameworks
@@ -13,7 +18,7 @@ fi
 if [ ! -d client/KarenMeu.app/Contents/Frameworks/VTextEdit.framework ]; then
 cp -R 3rdparty/vtextedit/src/editor/VTextEdit.framework client/KarenMeu.app/Contents/Frameworks/
 fi
-install_name_tool -change @executable_path/../Frameworks/ScintillaEdit.framework/Versions/4/ScintillaEdit @rpath/ScintillaEdit.framework/Versions/4/ScintillaEdit client/KarenMeu.app/Contents/MacOS/KarenMeu
+install_name_tool -change @executable_path/../Frameworks/ScintillaEdit.framework/Versions/5/ScintillaEdit @rpath/ScintillaEdit.framework/Versions/5/ScintillaEdit client/KarenMeu.app/Contents/MacOS/KarenMeu
 install_name_tool -change VSyntaxHighlighting.framework/Versions/1/VSyntaxHighlighting @rpath/VSyntaxHighlighting.framework/Versions/1/VSyntaxHighlighting client/KarenMeu.app/Contents/MacOS/KarenMeu 
 install_name_tool -change VTextEdit.framework/Versions/1/VTextEdit @rpath/VTextEdit.framework/Versions/1/VTextEdit client/KarenMeu.app/Contents/MacOS/KarenMeu
 otool -L client/KarenMeu.app/Contents/MacOS/KarenMeu
